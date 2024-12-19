28eb382a6cc16446e35fae0753326b4958f6c1e0
vicho314 <vicente.carrasco@usach.cl>
2024-12-18 23:12:09 -0300

    .gitignore + Inicio de TDA Board, Piece

--- conecta4/src/main/java/org/vicho314/tda/Board.java
+++ conecta4/src/main/java/org/vicho314/tda/Board.java
@@ -1,4 +1,38 @@
 package org.vicho314.tda;
 
+import org.vicho314.tda.Piece;
+//import java.util.ArrayList;
+
 public class Board {
+    private Piece[][] piezas;
+
+    public Board(Piece[][] piezas) {
+        this.piezas = piezas;
+    }
+    
+    public Board() {
+        this.piezas = new Piece[7][6];
+    }
+
+    public Piece[][] getPiezas() {
+        return piezas;
+    }
+
+    public void setPiezas(Piece[][] piezas) {
+        this.piezas = piezas;
+    }
+
+    public boolean inBounds(int x, int y){
+        boolean fila;
+        boolean columna;
+        columna = (x < 7) && (x >= 0);
+        fila = (y < 6) && (6 >= 0);
+        return (fila && columna);
+    }
+
+    public void setPiezasXY(Piece pieza, int x, int y) {
+        if(this.inBounds(x, y)) {
+            this.piezas[x][y] = pieza;
+        }
+    }
 }
--- conecta4/src/main/java/org/vicho314/tda/Piece.java
+++ conecta4/src/main/java/org/vicho314/tda/Piece.java
@@ -1,4 +1,17 @@
 package org.vicho314.tda;
 
 public class Piece {
+    private String color;
+
+    public Piece(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
